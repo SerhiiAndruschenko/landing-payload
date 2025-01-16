13736ef260a8bf761ec110a12746fe71981d705e
SergWebAcceptance <serg.web@acceptance.ltd>
2025-01-16 15:21:24 +0200

    test

--- src/app/(frontend)/page.jsx
+++ src/app/(frontend)/page.jsx
@@ -1,23 +1,9 @@
-async function fetchHomeData() {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_PAYLOAD_URL}/api/globals/home`
-    );
-    const homeData = await response.json();
-    console.log(homeData);
-    return homeData;
-  }
-  
-  const LandingPage = async () => {
-    const homeData = await fetchHomeData();
-  
-    return (
-      <div>
-        <h1>{homeData.title}</h1>
-        <p>{homeData.content}</p>
-        {homeData.image && <img src={homeData.image.url} alt={homeData.title} />}
-      </div>
-    );
-  };
-  
-  export default LandingPage;
-  
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default LandingPage
