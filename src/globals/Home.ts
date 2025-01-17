48fe681ab22c43f2b2a0bd560958bdd2fa0fc7f8
SergWebAcceptance <serg.web@acceptance.ltd>
2025-01-17 23:59:32 +0200

    fixed cache system

--- src/globals/Home.ts
+++ src/globals/Home.ts
@@ -1,10 +1,11 @@
 import type { GlobalConfig } from 'payload'
+import axios from 'axios'
 
 const Home: GlobalConfig = {
   slug: 'home',
   label: 'Головна сторінка',
   access: {
-    read: () => true, // Дозволяє читання всім
+    read: () => true,
   },
   fields: [
     {
@@ -23,10 +24,31 @@ const Home: GlobalConfig = {
       name: 'image',
       type: 'upload',
       label: 'Зображення',
-      relationTo: 'media', // Пов'язуємо з колекцією `Media`
+      relationTo: 'media',
       required: false,
     },
   ],
+  hooks: {
+    afterChange: [
+      async () => {
+        try {
+          const vercelToken = process.env.VERCEL_TOKEN
+          const vercelProject = process.env.VERCEL_PROJECT
+
+          const url = `https://api.vercel.com/v1/projects/${vercelProject}/cache`
+          await axios.delete(url, {
+            headers: {
+              Authorization: `Bearer ${vercelToken}`,
+            },
+          })
+
+          console.log(`Cache for project ${vercelProject} cleared successfully.`)
+        } catch (error) {
+          console.error('Error clearing Vercel cache:', error)
+        }
+      },
+    ],
+  },
 }
 
 export default Home
